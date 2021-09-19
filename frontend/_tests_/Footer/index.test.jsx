55b626ee3d018f9c2704fc4fb60acd6b64bc5496
Narbeh Shahnazarian <megatron@Narbehs-MacBook-Pro.local>
2021-09-18 17:02:12 -0700

    Adds unit testing framework
    - Adds jest and react-test-library as main unit testing framework
    - Adds test script and modifies build script to run tests first before actually building
    - Added jest config file following next.js instructions: https://nextjs.org/docs/testing
    - Added Footer unit tests

---
+++ frontend/_tests_/Footer/index.test.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Footer from '../../src/components/Footer/index';
+import '@testing-library/jest-dom';
+
+describe('Footer', () => {
+  it('Renders the github link', () => {
+    render(<Footer />);
+
+    const github = screen.getByTitle('Developer DAO GitHub organization');
+
+    testCommonLink(github, 'https://github.com/Developer-DAO');
+  });
+
+  it('Renders the icons', () => {
+    render(<Footer />);
+
+    const openSea = screen.getByTitle('OpenSea');
+    const etherscan = screen.getByTitle('Etherscan');
+    const twitter = screen.getByTitle('Twitter');
+    const discord = screen.getByTitle('Discord');
+
+    testCommonLink(
+      openSea,
+      'https://opensea.io/collection/devs-for-revolution',
+    );
+    testCommonLink(
+      etherscan,
+      'https://etherscan.io/address/0x25ed58c027921e14d86380ea2646e3a1b5c55a8b',
+    );
+    testCommonLink(twitter, 'https://twitter.com/developer_dao');
+    testCommonLink(discord, 'https://discord.gg/zRnZpzzY');
+  });
+
+  it('Renders the Vercel link', () => {
+    render(<Footer />);
+
+    const vercel = screen.getByTitle('Vercel');
+    testCommonLink(
+      vercel,
+      'https://vercel.com?utm_source=developerdao&utm_campaign=oss',
+    );
+  });
+});
+
+function testCommonLink(element, linkAddress) {
+  expect(element).toBeInTheDocument();
+  expect(element.closest('a')).toHaveAttribute('href', linkAddress);
+}
