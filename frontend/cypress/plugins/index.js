200fd90000406bfa32f960e9e9a2e1a2587d09fd
Filipe Oliveira <hi@fmoliveira.dev>
2021-09-19 20:42:52 -0300

    Install cypress plugin for clipboard testing

--- frontend/cypress/plugins/index.js
+++ frontend/cypress/plugins/index.js
@@ -1,9 +1,15 @@
 /// <reference types="cypress" />
 
+const clipboardy = require('clipboardy');
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  on('task', {
+    getClipboard: () => clipboardy.readSync(),
+  });
 };
