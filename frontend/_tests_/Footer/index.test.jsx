5ff3831ffc26aa95a75f235211bbbb084d523fd1
Narbeh Shahnazarian <megatron@Narbehs-MacBook-Pro.local>
2021-09-18 21:43:32 -0700

    Removes redundent link tests from footer

--- frontend/_tests_/Footer/index.test.jsx
+++ frontend/_tests_/Footer/index.test.jsx
@@ -10,7 +10,7 @@ describe('Footer', () => {
 
     const github = screen.getByTitle('Developer DAO GitHub organization');
 
-    testCommonLink(github, 'https://github.com/Developer-DAO');
+    expect(github).toBeInTheDocument();
   });
 
   it('Renders the icons', () => {
@@ -21,25 +21,17 @@ describe('Footer', () => {
     const twitter = screen.getByTitle('Twitter');
     const discord = screen.getByTitle('Discord');
 
-    testCommonLink(
-      openSea,
-      'https://opensea.io/collection/devs-for-revolution',
-    );
-    testCommonLink(
-      etherscan,
-      'https://etherscan.io/address/0x25ed58c027921e14d86380ea2646e3a1b5c55a8b',
-    );
-    testCommonLink(twitter, 'https://twitter.com/developer_dao');
-    testCommonLink(discord, 'https://discord.gg/zRnZpzzY');
+    expect(openSea).toBeInTheDocument();
+    expect(etherscan).toBeInTheDocument();
+    expect(twitter).toBeInTheDocument();
+    expect(discord).toBeInTheDocument();
   });
 
   it('Renders the Vercel link', () => {
     render(<Footer />);
 
     const vercel = screen.getByTitle('Vercel');
-    testCommonLink(
-      vercel,
-      'https://vercel.com?utm_source=developerdao&utm_campaign=oss',
-    );
+
+    expect(vercel).toBeInTheDocument();
   });
 });
