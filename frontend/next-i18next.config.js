1f886cf06c72592bf8e3cd355ff992aea259aa45
Filipe Oliveira <hi@fmoliveira.dev>
2021-09-19 00:39:19 -0300

    Copy pt-BR translation to portuguese fallback

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'es', 'fr', 'pt-BR', 'ru', 'zh', 'zh-CN', 'zh-HK', 'zh-TW'],
+    locales: [
+      'en',
+      'es',
+      'fr',
+      'pt',
+      'pt-BR',
+      'ru',
+      'zh',
+      'zh-CN',
+      'zh-HK',
+      'zh-TW',
+    ],
   },
 };
