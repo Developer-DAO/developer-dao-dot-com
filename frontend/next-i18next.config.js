3b1923f56142aa9aa39e413f36ddc68de35c2e0e
Filipe Oliveira <hi@fmoliveira.dev>
2021-09-19 00:38:20 -0300

    Add translation to pt-BR

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'es', 'fr', 'ru', 'zh', 'zh-CN', 'zh-HK', 'zh-TW'],
+    locales: ['en', 'es', 'fr', 'pt-BR', 'ru', 'zh', 'zh-CN', 'zh-HK', 'zh-TW'],
   },
 };
