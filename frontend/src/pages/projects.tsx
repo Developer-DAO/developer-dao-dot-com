df451ebef67d9cf503a7d8b3ba776c4cfcc5190f
Davy Peter BRAUN <hello@davybraun.com>
2021-09-25 19:09:17 +0200

    feat(i18n): add all text to i18n, translate fr locale

--- frontend/src/pages/projects.tsx
+++ frontend/src/pages/projects.tsx
@@ -19,21 +19,17 @@ function Projects() {
     <PageLayout>
       <chakra.main>
         <Box mx="auto" maxW="6xl" py={3} px={4}>
-          <Text fontSize="xl">Projects</Text>
+          <Text fontSize="xl">{t('projects')}</Text>
           <Text my={2} color="gray.600">
-            {t('projects')}
+            {t('projectsList')}
           </Text>
           <UnorderedList spacing="1">
             <ListItem>
-              <Link href="https://ddao.ibby.dev/">DDAO Token Search</Link> -{' '}
+              <Link href="https://ddao.ibby.dev/">{t('ddaoTokenSearch')}</Link> -{' '}
               {t('by')}{' '}
               <Link href="https://github.com/Ibby-devv">Brian Eter</Link>
-            </ListItem>
-            <ListItem>
-              <Link href="https://developerdao.vercel.app/ ">
-                Developer DAO
-              </Link>{' '}
-              - {t('by')}{' '}
+              <Link href="https://developerdao.vercel.app/ ">{t('title')}</Link> -{' '}
+              {t('by')}{' '}
               <Link href="https://github.com/fmoliveira">fmoliveira</Link>
             </ListItem>
           </UnorderedList>
