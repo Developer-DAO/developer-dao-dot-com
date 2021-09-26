bbac0663f02a1112dab8a81275bd8ad10eec81a2
Nagma Kapoor <nagmakapoor@gmail.com>
2021-09-26 16:36:43 -0400

    fixed layout

--- frontend/src/pages/mint.tsx
+++ frontend/src/pages/mint.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import DirectMintBox from '../components/DirectMint/DirectMintBox';
-import Nav from '../components/Nav';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import PageLayout from '../layout/Page';
+import { useTranslation } from 'react-i18next';
+import { chakra } from '@chakra-ui/react';
 
 const Mint = () => {
+  const { t } = useTranslation();
   return (
     <>
-      <Nav />
-      <DirectMintBox />
+      <PageLayout>
+        <chakra.main>
+          <DirectMintBox />
+        </chakra.main>
+      </PageLayout>
     </>
   );
 };
+
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common'])),
+  },
+});
 
 export default Mint;
