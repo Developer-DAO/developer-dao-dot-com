9aefa9a7d3b157a4b5e82c5ea11fd62b5d1b69c8
Alex Kam <kavimaluskam@gmail.com>
2021-11-07 14:06:47 +0100

    Cast env var to string for typescript

--- frontend/src/utils/DeveloperDaoConstants.ts
+++ frontend/src/utils/DeveloperDaoConstants.ts
@@ -1,25 +1,26 @@
-export const DEVELOPER_DAO_CONTRACT =
-  process.env.NEXT_PUBLIC_DEVELOPER_DAO_CONTRACT;
-export const DEVELOPER_DAO_CONTRACT_NETWORK =
-  process.env.NEXT_PUBLIC_DEVELOPER_DAO_CONTRACT_NETWORK;
-export const DEVELOPER_DAO_CONTRACT_NETWORK_PROVIDER =
-  process.env.NEXT_PUBLIC_DEVELOPER_DAO_CONTRACT_NETWORK;
+export const DEVELOPER_DAO_CONTRACT = process.env
+  .NEXT_PUBLIC_DEVELOPER_DAO_CONTRACT as string;
+export const DEVELOPER_DAO_CONTRACT_NETWORK = process.env
+  .NEXT_PUBLIC_DEVELOPER_DAO_CONTRACT_NETWORK as string;
+export const DEVELOPER_DAO_CONTRACT_NETWORK_PROVIDER = process.env
+  .NEXT_PUBLIC_DEVELOPER_DAO_CONTRACT_NETWORK as string;
 export const NETWORK_ID = process.env.NEXT_PUBLIC_NETWORK_ID;
 export const DEVELOPER_DAO_WEBSITE = 'https://www.developerdao.com';
-export const OPENSEA_DIRECT_LINK_PREFIX =
-  process.env.NEXT_PUBLIC_OPENSEA_DIRECT_LINK_PREFIX;
-export const OPENSEA_COLLECTION_LINK =
-  process.env.NEXT_PUBLIC_OPENSEA_COLLECTION_LINK;
-export const ETHER_SCAN_LINK_PREFIX =
-  process.env.NEXT_PUBLIC_ETHER_SCAN_LINK_PREFIX;
+export const OPENSEA_DIRECT_LINK_PREFIX = process.env
+  .NEXT_PUBLIC_OPENSEA_DIRECT_LINK_PREFIX as string;
+export const OPENSEA_COLLECTION_LINK = process.env
+  .NEXT_PUBLIC_OPENSEA_COLLECTION_LINK as string;
+export const ETHER_SCAN_LINK_PREFIX = process.env
+  .NEXT_PUBLIC_ETHER_SCAN_LINK_PREFIX as string;
 
 export const SITE_URL = 'https://developerdao.com';
 export const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
 
 export const TOKEN_FINDER_URL = 'https://developerdao.vercel.app/';
-export const ETHERSCAN_TX_URL = process.env.NEXT_PUBLIC_ETHERSCAN_TX_URL;
+export const ETHERSCAN_TX_URL = process.env
+  .NEXT_PUBLIC_ETHERSCAN_TX_URL as string;
 
-export const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_ID;
+export const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_ID as string;
 
 export const DEVELOPER_DAO_CONTRACT_ABI = [
   { inputs: [], stateMutability: 'nonpayable', type: 'constructor' },
