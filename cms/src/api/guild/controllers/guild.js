de34afd0f87b1c30ad3d527ceb4239f0e22eb8b7
Matthew Mulford <mhmulford0@gmail.com>
2022-07-05 15:56:40 -0500

    update models

---
+++ cms/src/api/guild/controllers/guild.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  guild controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::guild.guild');
---
+++ cms/src/api/guild/routes/guild.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * guild router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::guild.guild');
