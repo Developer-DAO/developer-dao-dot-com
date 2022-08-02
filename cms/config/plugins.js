30fb4fc916eca60f781f6d67059626f6d0e022b3
mulford.eth <63870279+mhmulford0@users.noreply.github.com>
2022-08-02 15:32:33 -0500

    rotate key (#10)

--- cms/config/plugins.js
+++ cms/config/plugins.js
@@ -4,8 +4,8 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: "DO004M8VR6Y63M4TBZVW", // env("SCALEWAY_ACCESS_KEY_ID"),
-        secretAccessKey: "lvnAQwamgpmuhkULWnCsjjl3HtEnj6uGcCTkgxGov9M", // env("SCALEWAY_ACCESS_SECRET"),
+        accessKeyId: env("STORAGE_ACCESS_KEY_ID"), // env("SCALEWAY_ACCESS_KEY_ID"),
+        secretAccessKey: env("STORAGE_SECRET_KEY"), // env("SCALEWAY_ACCESS_SECRET"),
         endpoint: "ams3.digitaloceanspaces.com", // env("SCALEWAY_ENDPOINT"), // e.g. "s3.fr-par.scw.cloud"
         params: {
           Bucket: "sitemedia", //env("SCALEWAY_BUCKET"),
