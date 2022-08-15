47daa13010c0527535ff0208c562b8d8883f76b7
WIll <kempsterwilliam@gmail.com>
2022-08-15 01:08:24 +0100

    adding locale to terms and privacy pages

--- packages/frontend/src/pages/terms.tsx
+++ packages/frontend/src/pages/terms.tsx
@@ -489,7 +489,7 @@ export default function TermsPage({
   );
 }
 
-export const getStaticProps = async () => {
+export const getStaticProps = async ({ locale }: { locale: string }) => {
   const { data } = await client.query({
     query: gql`
       query TermsPage {
