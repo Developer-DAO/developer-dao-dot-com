52548e50739baf4be15aa57fe9df7ecccdf49391
Matthew Mulford <mhmulford0@gmail.com>
2022-08-17 16:35:10 -0500

    new deploy pipeline

---
+++ packages/frontend/scripts/deploy.mjs
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
+
+async function main() {
+  try {
+    console.log('\n');
+    console.log('🚨🚨 Frontend Deploy In Progress 🚨🚨');
+    console.log('\n');
+    await axios.post(process.env.VERCEL_WEBHOOK_URL);
+
+    console.log('🚨🚨 Frontend Deploy Started 🚨🚨');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main()
+  .then()
+  .catch(() => process.exit(1));
