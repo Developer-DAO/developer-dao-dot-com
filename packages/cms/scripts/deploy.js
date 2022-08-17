361eca661400f729697deaf69351b5edb5bae279
Matthew Mulford <mhmulford0@gmail.com>
2022-08-17 16:22:48 -0500

    new deploy pipeline

--- packages/cms/scripts/deploy.js
+++ packages/cms/scripts/deploy.js
@@ -1,5 +1,6 @@
 // deploy to DO as part of CI
 const axios = require('axios').default;
+require('dotenv').config();
 
 async function main() {
   try {
@@ -14,11 +15,12 @@ async function main() {
         },
       }
     );
+
     const deployId = data.deployment.id;
-    console.log(deployId);
-    console.log(
-      `https://api.digitalocean.com/v2/apps/21030157-a72b-40d2-afa6-0b498c63144d/deployments/${deployId}`
-    );
+
+    console.log('\n');
+    console.log('🚨🚨 CMS Deploy In Progress 🚨🚨');
+    console.log('\n');
 
     const waitForDeploy = setInterval(async () => {
       const deployData = await axios(
@@ -26,14 +28,14 @@ async function main() {
         {
           headers: {
             'Content-Type': 'application/json',
-            Authorization:
-              'Bearer dop_v1_e71d4dfae7bc57d9915a696ee55cbe2f7d8a428898c6484d0c9f16a0ba0b3cd2',
+            Authorization: `Bearer ${process.env.CMS_DEPLOY_KEY}`,
           },
         }
       );
       if (deployData.data.deployment.phase === 'ACTIVE') {
         clearInterval(waitForDeploy);
         console.log('CMS DEPLOY COMPLETE 🐐🎉🚀');
+        console.log('\n');
       }
     }, 1500);
   } catch (error) {
